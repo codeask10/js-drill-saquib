fe96682398ecb5a449926b3135a022ef8f1cf365
codeask10 <annajmussaquib123@gmail.com>
2024-09-04 12:37:22 +0530

    Implement function to get hobbies of individuals based on age

--- solution.js
+++ solution.js
@@ -8,6 +8,15 @@ function getAllEmails(arr){
     }
     return ans;
 }
-export default getAllEmails;
+//Implement a function that retrieves and prints the hobbies of individuals with a specific age, say 30 years old.
+function getHobbyByAge(arr,age){
+    for(const object in arr){
+        if(arr[object].age===age){
+            return arr[object].hobbies;
+        }
+    }
+    return "Specific age not found"
+}
+export  { getAllEmails,getHobbyByAge};
 
 
