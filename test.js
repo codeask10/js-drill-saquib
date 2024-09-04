9ff0dcd3d45e0838603fac4ef2436130cb72aa38
codeask10 <annajmussaquib123@gmail.com>
2024-09-04 12:20:54 +0530

    Add tests for email retrieval function with various cases

---
+++ test.js
@@ -0,0 +1,8 @@
+import arrayOfObjects from './person.js'
+import getAllEmails from "./solution.js";
+
+// // Test cases
+console.log(getAllEmails(arrayOfObjects));
+console.log(getAllEmails([]));
+
+
