4c828915616521e5f0d50750cc663a2ea98a7066
codeask10 <annajmussaquib123@gmail.com>
2024-09-04 14:12:27 +0530

    Add tests for Australian students name extraction function

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
 import arrayOfObjects from './person.js'
-import {getAllEmails,getHobbyByAge} from "./solution.js";
+import {getAllEmails,getHobbyByAge, getStudentName} from "./solution.js";
 
 
 //   Test cases
-console.log(getAllEmails(arrayOfObjects));
-console.log(getAllEmails([]));
+console.log(getAllEmails(arrayOfObjects));  //['alice@example.com', 'bob@example.com', 'charlie@example.com', 'david@example.com', 'eve@example.com', 'frank@example.com', 'grace@example.com', 'hannah@example.com', 'ivy@example.com', 'jack@example.com']
+console.log(getAllEmails([])); // []
 
-console.log(getHobbyByAge(arrayOfObjects,25));
-console.log(getHobbyByAge(arrayOfObjects,100));
+console.log(getHobbyByAge(arrayOfObjects,25));   // ['playing guitar', 'hiking']
+console.log(getHobbyByAge(arrayOfObjects,100));  //Specific age not found
+
+console.log(getStudentName(arrayOfObjects,"Australia")); // Hannah
+console.log(getStudentName(arrayOfObjects,"Canada")); //Name not found
+console.log(getStudentName()); //Name not found
+
 
 
 
