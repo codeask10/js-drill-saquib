243530d121c16ea2ca4dbe8e4c04eed02642e895
codeask10 <annajmussaquib123@gmail.com>
2024-09-05 11:06:07 +0530

    Add Javascript Drill 2 folder, includes solution,test and user file

---
+++ Javascript_Drill_2/user.js
@@ -0,0 +1,49 @@
+const users = {
+    "John": {
+        age: 24,
+        desgination: "Senior Golang Developer",
+        interests: ["Chess, Reading Comics, Playing Video Games"],
+        qualification: "Masters",
+        nationality: "Greenland"
+    },
+    "Ron": {
+        age: 19,
+        desgination: "Intern - Golang",
+        interests: ["Video Games"],
+        qualification: "Bachelor",
+        nationality: "UK"
+    },
+    "Wanda": {
+        age: 24,
+        desgination: "Intern - Javascript",
+        interests: ["Piano"],
+        qualification: "Bachaelor",
+        nationality: "Germany"
+    },
+    "Rob": {
+        age: 34,
+        desgination: "Senior Javascript Developer",
+        interests: ["Walking his dog, Cooking"],
+        qualification: "Masters",
+        nationality: "USA"
+    },
+    "Pike": {
+        age: 23,
+        desgination: "Python Developer",
+        interests: ["Listing Songs, Watching Movies"],
+        qualification: "Bachaelor's Degree",
+        nationality: "Germany"
+    }
+}
+let ans=[];
+for(const obj in users){
+    for(let i =0;i<users[obj].interests.length;i++){
+        if(users[obj].interests[i].toLowerCase().includes("video game")){
+            console.log(obj);
+            ans.push(obj);
+        }
+    }
+
+}
+console.log(ans);
+
