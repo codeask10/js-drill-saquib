9dddc07f73213a77df0287673d21a3e36d942b50
codeask10 <annajmussaquib123@gmail.com>
2024-09-06 16:36:58 +0530

    Add test cases for extracting car years from inventory for Problem #4

--- Javascript_Drill_3/test.js
+++ Javascript_Drill_3/test.js
@@ -1,5 +1,5 @@
 import inventory from './car.js';
-import { getCarById, getLastCarDetails,getSortedCarModelsAlphabetically} from './solution.js';
+import { getCarById, getLastCarDetails,getSortedCarModelsAlphabetically,getCarYears} from './solution.js';
 
 //------------------------> ### Test Case ### <----------------------------------
 
@@ -29,3 +29,12 @@ if (typeof lastCar === 'object' && lastCar !== null) {
 // The marketing team wants the car models listed alphabetically on the website. Execute a function to Sort all the car model names into alphabetical order and log the results in the console as it was returned.
 
 getSortedCarModelsAlphabetically(inventory);
+
+// ==== Problem #4 ====
+// The accounting team needs all the years from every car on the lot. Execute a function that will return an array from the dealer data containing only the car years and log the result in the console as it was returned.
+const carYear=getCarYears(inventory);
+if (typeof carYear === 'array' && carYear !== null) {
+    console.log(carYear)
+} else {
+    console.log(carYear); // Handle the error message returned
+}
