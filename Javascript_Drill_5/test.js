49327aba0b602d0236871e5f98ed60565c2c3473
codeask10 <annajmussaquib123@gmail.com>
2024-09-09 16:11:17 +0530

    Add test case for values function to log property values from testObject

--- Javascript_Drill_5/test.js
+++ Javascript_Drill_5/test.js
@@ -1,5 +1,7 @@
-import { keys } from "./objects.js";
+import { keys, values } from "./objects.js";
 
 let testObject ={ name: "Bruce Wayne", age: 36, location: "Gotham" }; 
 
 console.log(keys(testObject));
+
+console.log(values(testObject));
