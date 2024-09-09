4e0cd1f96212dbcff60be42fa966f9bca9c5ccbd
codeask10 <annajmussaquib123@gmail.com>
2024-09-09 11:39:33 +0530

    Integrate and test each function with eachCallbackFn using sample array

---
+++ Javascript_Drill_4/test.js
@@ -0,0 +1,7 @@
+//-----------------------------> Test Cases <------------------------------------------
+const items = [1, 2, 3, 4, 5, 5]; // use this array to test your code.
+
+import eachCallbackFn from "./callbackFn.js";
+import each from "./arrays.js";
+
+each(items,eachCallbackFn);
