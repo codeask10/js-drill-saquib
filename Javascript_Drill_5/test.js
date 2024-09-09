93dde76947a15d896e162b1f58d4e1b2964529ac
codeask10 <annajmussaquib123@gmail.com>
2024-09-09 16:15:27 +0530

    Add test case for mapObject function to log transformed testObject values using mapCallbackFn

--- Javascript_Drill_5/test.js
+++ Javascript_Drill_5/test.js
@@ -1,7 +1,14 @@
-import { keys, values } from "./objects.js";
+import { keys, mapObject, values } from "./objects.js";
 
 let testObject ={ name: "Bruce Wayne", age: 36, location: "Gotham" }; 
 
 console.log(keys(testObject));
 
 console.log(values(testObject));
+
+//map callback function 
+function mapCallbackFn(value, key, obj) {
+    return value + 5;
+}
+console.log(mapObject(testObject,mapCallbackFn));
+
