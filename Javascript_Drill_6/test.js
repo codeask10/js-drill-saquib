1569624650c9c625ad00ab4263a301f9a7efa85b
codeask10 <annajmussaquib123@gmail.com>
2024-09-10 15:26:59 +0530

    Implement test case to verify function call limit functionality

--- Javascript_Drill_6/test.js
+++ Javascript_Drill_6/test.js
@@ -1,10 +1,28 @@
 //--------------------->TEST CASES <-----------------------------------------------------
 
-import { counterFactory } from "./closures.js";
+import { counterFactory, limitFunctionCallCount } from "./closures.js";
 
-const obj = counterFactory(5);
-console.log(obj.increment()) // 6
-console.log(obj.increment()) // 7
-console.log(obj.decrement()) // 6
-console.log(obj.increment()) // 7
-console.log(obj.increment()) // 8
+// const obj = counterFactory(5);
+// console.log(obj.increment()) // 6
+// console.log(obj.increment()) // 7
+// console.log(obj.decrement()) // 6
+// console.log(obj.increment()) // 7
+// console.log(obj.increment()) // 8
+
+function primeNumber(num) {
+    if (num == 0 || num == 1) return false;
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Usage example:
+const limitedFunction = limitFunctionCallCount(primeNumber, 3);
+
+console.log(limitedFunction(7)); // Output: true;
+console.log(limitedFunction(2)); // Output: true
+console.log(limitedFunction(6)); // Output: false
+console.log(limitedFunction(5)); // Output: null (limit reached)
