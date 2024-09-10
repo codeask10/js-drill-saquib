0623fe7816e4a3f8e4f7dc31ab5d2f562a9d88db
codeask10 <annajmussaquib123@gmail.com>
2024-09-10 16:01:42 +0530

    Add test case to ensure caching function avoids redundant calls

--- Javascript_Drill_6/test.js
+++ Javascript_Drill_6/test.js
@@ -1,13 +1,13 @@
 //--------------------->TEST CASES <-----------------------------------------------------
 
-import { counterFactory, limitFunctionCallCount } from "./closures.js";
+import { counterFactory, limitFunctionCallCount,cacheFunction } from "./closures.js";
 
-// const obj = counterFactory(5);
-// console.log(obj.increment()) // 6
-// console.log(obj.increment()) // 7
-// console.log(obj.decrement()) // 6
-// console.log(obj.increment()) // 7
-// console.log(obj.increment()) // 8
+const obj = counterFactory(5);
+console.log(obj.increment()) // 6
+console.log(obj.increment()) // 7
+console.log(obj.decrement()) // 6
+console.log(obj.increment()) // 7
+console.log(obj.increment()) // 8
 
 function primeNumber(num) {
     if (num == 0 || num == 1) return false;
@@ -24,5 +24,17 @@ const limitedFunction = limitFunctionCallCount(primeNumber, 3);
 
 console.log(limitedFunction(7)); // Output: true;
 console.log(limitedFunction(2)); // Output: true
-console.log(limitedFunction(6)); // Output: false
+console.log(limitedFunction(6)); // Output: false       
 console.log(limitedFunction(5)); // Output: null (limit reached)
+
+
+function multipleOf3(num){
+    return num*3;
+}
+
+const  cache=cacheFunction(multipleOf3);
+console.log(cache(1)); //Calculating result: 3
+console.log(cache(2)); //Calculating result: 6
+console.log(cache(3)); //Calculating result: 9
+console.log(cache(4)); //Calculating result: 12
+console.log(cache(2)); //Returning cached result: 6
