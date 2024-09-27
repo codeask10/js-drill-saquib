de1f6b0e61cbaa63766f40cb701228306bbbc23a
codeask10 <annajmussaquib123@gmail.com>
2024-09-27 10:12:47 +0530

    Calculate the sum of resolved values using Promise.all to handle multiple promises concurrently

---
+++ Javascript_Drill_8/Excercise/BONUS-exercise.js
@@ -0,0 +1,37 @@
+/*
+    1. Create a Promise that resolves with the number 10 after
+       3 seconds
+    2. Create another Promise that resolves with the number
+       20 after 5 seconds
+
+    How can we log out the sum (30) of these two resolved values
+    once, after BOTH promises successfully fulfill?
+
+    HINT: Use Google/Documentation to help find an answer
+    HINT2: You can Google for something like:
+           "resolve 2 promises at the same time javascript"
+*/
+
+const promise1= new Promise((resolve,reject)=>{
+   setTimeout(() => {
+      resolve(10);
+   }, 3000);
+});
+
+const promise2= new Promise((resolve,reject)=>{
+   setTimeout(() => {
+      resolve(20);
+   }, 5000);
+});
+
+const promise=[promise1,promise2];
+
+Promise.all(promise)
+.then((result)=>{
+   console.log(result);
+   const sum =result.reduce((accumulator,val)=>{
+      accumulator+=val;
+      return accumulator;
+   },0);
+   console.log(sum);
+})
